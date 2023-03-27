f7a8f6ab9f1c1f7f5eb3b5855aea17395f39a3f7
Nhat Nguyen Dang <105844648+Nhat-NguyenDang@users.noreply.github.com>
2023-03-27 17:22:15 +0900

    Add n_clusters input

--- my_app.py
+++ my_app.py
@@ -11,8 +11,8 @@ st.header("This program cluster answers")
 
 with st.form("my-form", clear_on_submit=True):
     input_file = st.file_uploader("Drop your file here")
+    n_clusters = st.number_input("Input how many clusters you want:", max_value=10, min_value=1, step=1)
     submitted = st.form_submit_button("UPLOAD and CLUSTER")
-
 
 def save_uploadedfile(uploadedfile):
      with open(os.path.join("tempDir",uploadedfile.name),"wb") as f:
@@ -25,10 +25,10 @@ if submitted and input_file is not None:
     placeholder.text("Please Wait")
     save_uploadedfile(input_file)
     file_path = os.path.join("tempDir", input_file.name)
-    new_file = file_path.replace('.xlsx', '')+"_clustered.xlsx"
+    new_file = file_path.replace('.xlsx', '')+f"_{n_clusters}clustered.xlsx"
     shutil.copy(file_path, new_file)
     data_preprocessed = preprocess_data(new_file)
-    write_result(data_preprocessed, new_file)
+    write_result(data_preprocessed, new_file, n_clusters=n_clusters)
     with open(new_file, 'rb') as f:
         st.download_button('Download result', data=f, file_name=pathlib.PurePath(new_file).name)
     placeholder.text("DONE!")
